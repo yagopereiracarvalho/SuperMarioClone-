4a08fbd2c0d876aac94753f1ee856dc3212d3db1
yagopereiracarvalho <yagopereiracarvalho72@gmail.com>
2026-05-15 14:13:01 -0300

    Criação das outras Versoens Do Mario

--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
+    [SerializeField] int layerWeight;
     Animator anim;
+
+    public int LayerWeight {get => layerWeight;  set => layerWeight = value;}
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,7 @@ public class PlayerAnim : MonoBehaviour
     void Update()
     {
        ChangeAnimation(); 
+       changelayer();
     }
     
     void ChangeAnimation()
@@ -30,6 +34,25 @@ public class PlayerAnim : MonoBehaviour
             {
                  anim.SetInteger("Transition", 1);
             }
+        }
+    }
+
+    void changelayer()
+    {
+        if(layerWeight == 0)
+        {
+            anim.SetLayerWeight(1,0);
+            anim.SetLayerWeight(2,0);
+        }
+        else if(layerWeight == 1)
+        {
+            anim.SetLayerWeight(1,1);
+            anim.SetLayerWeight(2,0);
+        }
+        else if(layerWeight == 2)
+        {
+             anim.SetLayerWeight(1,0);
+            anim.SetLayerWeight(2,1);
         }
     }
     
