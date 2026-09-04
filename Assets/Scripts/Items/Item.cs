71d43b4b57c6e1d0f0ec189270cbd314e9f61b8b
yagopereiracarvalho <yagopereiracarvalho72@gmail.com>
2026-09-04 14:39:48 -0300

    Criação do Script Padrão

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,43 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] float StartPos; 
+    float posY;
+    protected float direction;
+    protected bool isStarted; 
+    BoxCollider2D boxCollider;
+    public float Direction {get => direction; set => direction = value;}
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetValues(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Started(); 
+    }
+    protected void Started()
+    {
+        if(transform.position.y < posY && !isStarted)
+        {
+         transform.Translate(Vector3.up *Time.deltaTime);
+        }
+        else
+        {
+            boxCollider.enabled = true; 
+            isStarted = true;
+        }
+    }
+    protected void SetValues()
+    {
+        posY = transform.position.y + StartPos;
+        boxCollider = GetComponent<BoxCollider2D>();
+        boxCollider.enabled = false; 
+    }
+}
